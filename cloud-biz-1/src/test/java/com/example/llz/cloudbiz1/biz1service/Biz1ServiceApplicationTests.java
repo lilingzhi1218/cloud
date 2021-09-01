d15a1cfb8081683db78003dec2262880fe771bb6
lilingzhi <lingzhi.li@southgis.com>
2021-09-01 11:22:47 +0800

    begin again

---
+++ cloud-biz-1/src/test/java/com/example/llz/cloudbiz1/biz1service/Biz1ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.llz.cloudbiz1.biz1service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Biz1ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
