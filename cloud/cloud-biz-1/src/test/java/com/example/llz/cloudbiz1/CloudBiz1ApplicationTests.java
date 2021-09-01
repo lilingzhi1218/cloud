5e5b609d26f80d4313c9e12fe39e80d50253e2d0
lilingzhi1218 <67905996+lilingzhi1218@users.noreply.github.com>
2021-09-01 11:17:09 +0800

    begin

---
+++ cloud/cloud-biz-1/src/test/java/com/example/llz/cloudbiz1/CloudBiz1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.llz.cloudbiz1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudBiz1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
