ab7fd628426347770bfef5081c152f07f461fe28
lilingzhi <lingzhi.li@southgis.com>
2021-09-09 17:03:03 +0800

    [*]master-1

--- cloud-biz-1/src/main/java/com/example/llz/cloudbiz1/utils/CheckUtil.java
+++ cloud-biz-1/src/main/java/com/example/llz/cloudbiz1/utils/CheckUtil.java
@@ -42,7 +42,7 @@ public final class CheckUtil {
                 if (oVal == item) {
                     return true;
                 }
-            } while(oVal == null || !oVal.equals(item));
+            } while(oVal == null || !oVal.equals(item));   
 
             return true;
         }
