6a94ce9cab7984f21a59fcb606af32c5a2577a0f
lilingzhi <lingzhi.li@southgis.com>
2021-09-29 16:39:14 +0800

    Mybatis通过注解查询数据库

--- cloud-biz-1/src/main/java/com/example/llz/cloudbiz1/dao/PersonDao.java
+++ cloud-biz-1/src/main/java/com/example/llz/cloudbiz1/dao/PersonDao.java
@@ -3,7 +3,10 @@ package com.example.llz.cloudbiz1.dao;
 import com.example.llz.cloudbiz1.entity.Person;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
@@ -12,4 +15,7 @@ public interface PersonDao {
     Person findPersonByName(@Param("name")String name);
     
     Integer addPerson(Person person);
+    
+    @Select("select * from person")
+    List<Person> findAllPerson();
 }
