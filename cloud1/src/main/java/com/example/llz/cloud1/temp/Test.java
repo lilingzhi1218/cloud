6212d37658af6ab01a1836132ac8cc10110512e1
lilingzhi <lingzhi.li@southgis.com>
2022-02-26 19:21:32 +0800

    git测试

--- cloud1/src/main/java/com/example/llz/cloud1/temp/Test.java
+++ cloud1/src/main/java/com/example/llz/cloud1/temp/Test.java
@@ -13,4 +13,5 @@ public class Test<E> extends ExtendTest<E> implements InterfaceTest1, InterfaceT
     public static void main(String[] args) {
         Test<String> test =  new Test<>();
     }
+    ///master-1
 }
