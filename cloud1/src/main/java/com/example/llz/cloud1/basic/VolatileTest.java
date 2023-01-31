3fdd625ad9282f8a36c077d1ef3ead270a8a0f0f
lilingzhi <782834159@qq.com>
2023-01-31 14:46:06 +0800

    单例模式的懒汉式创建

---
+++ cloud1/src/main/java/com/example/llz/cloud1/basic/VolatileTest.java
@@ -0,0 +1,29 @@
+package com.example.llz.cloud1.basic;
+
+public class VolatileTest {
+	private volatile Boolean flag = true;
+	
+	private void updateFlag(){
+		System.out.println("线程" + Thread.currentThread().getName() + "把flag的值更改成了false");
+		flag = false;
+	}
+
+	public static void main(String[] args) {
+		VolatileTest volatileTest = new VolatileTest();
+		new Thread(()->{
+			while (volatileTest.flag) {
+				
+			}
+			System.out.println(Thread.currentThread().getName() + "线程结束");
+		}).start();
+		
+		new Thread(()->{
+			try {
+				Thread.sleep(3000);
+				volatileTest.updateFlag();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}).start();
+	}
+}
